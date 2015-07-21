233c5fd56737db21836fd351b6e90e2382f5d676
dolphineor <dolphineor@gmail.com>
2015-07-21 10:15:36 +0800

    Change jade template code

--- src/main/scala/org/cgdol/app/HlScalatraServlet.scala
+++ src/main/scala/org/cgdol/app/HlScalatraServlet.scala
@@ -12,7 +12,11 @@ import scalate.ScalateSupport
 class HlScalatraServlet extends ScalatraStack with SlickSupport {
 
   get("/") {
-    <html>
+    <html lang="zh-cmn-Hans">
+      <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      </head>
       <body>
         <h1>Hello, world!</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
